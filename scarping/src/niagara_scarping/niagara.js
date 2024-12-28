5128c399080acd51ca97304acae7d9dfbf6b00d7
suresh04-tech <suresh.s@meyicloud.com>
2024-12-28 18:17:08 +0530

    niagara_webiste completed

--- scarping/src/niagara_scarping/niagara.js
+++ scarping/src/niagara_scarping/niagara.js
@@ -2,91 +2,102 @@ import fetch from 'node-fetch'
 import { parseStringPromise } from 'xml2js';
 import getuuid from 'uuid-by-string';
 
+const NIAGARA_FALLS_FIRE_ALERT_RSS_FEED = process.env.NIAGARA_FALLS_FIRE_ALERT_RSS_FEED ?? "http://localhost:4000/xml"
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY ?? "AIzaSyCf39T5pe1d25umPF98HvufsnZt4rE8vwo";
 
-const GOOGLE_API_KEY=process.env.GOOGLE_API_KEY ?? "AIzaSyCf39T5pe1d25umPF98HvufsnZt4rE8vwo";
 
-
-//getting xml file from API and convert into JSON
-async function fetchXmlConvertJson() {
-  const url = 'http://localhost:4000/xml';
-
+async function fetchDataFromFireAlertRSSFeed() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(NIAGARA_FALLS_FIRE_ALERT_RSS_FEED);
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`[NIAGARA_FIRE_ALERT_API] Cannot able to fetch response :${response.status}`);
     }
     const xmlData = await response.text();
-    console.log('Fetched XML Data:', xmlData);
 
     const jsonData = await parseStringPromise(xmlData);
     console.log('Parsed JSON Data:', jsonData);
+
     return jsonData;
-   
 
   } catch (error) {
-    console.error('Error fetching or parsing XML data:', error);
+    console.error('Error Try to fetching the data :', error);
   }
 }
 
 
-async function processAlert(input) {
-  console.log("file",input)
-  const seenTitles = new Set(); 
-  const uniqueTitles = []; 
-  const uniqueTimestamps = []; 
-  
-  if (input && input.feed && Array.isArray(input.feed.entry)) {
-    input.feed.entry.forEach((entry) => {
-      if (Array.isArray(entry.title)) {
-        entry.title.forEach((titleObj) => {
-          let entTitle = titleObj && titleObj._ ? titleObj._ : 'No Title';
-          const timestamp = entry.updated && entry.updated[0] ? entry.updated[0] : 'No Timestamp';
-  
-          entTitle = entTitle.replace("Active Fire Call at", "").trim();
-          let title = String(entTitle);
-  
-          if (!seenTitles.has(title)) {
-            seenTitles.add(title);
-            uniqueTitles.push(title);
-            uniqueTimestamps.push(timestamp);
-          }
-        });
-      } else {
-        console.warn('No valid "title" array found in entry');
-      }
-    });
-  } else {
-    console.error('No valid "entry" data found in the input');
+async function getFireAlertUniqueLocationTitle(input) {
+  const seenTitles = new Set();
+  const uniqueAlertTitles = [];
+
+  if (!input || !input.feed) {
+    console.log('No valid "entry" data found in the input');
+    return [];
   }
-  
 
-  try{
-    while(uniqueTitles.length>0){
-    //currentTitle
-    const currentTitle= uniqueTitles.shift();
+  const entries = input.feed.entry;
+  entries.forEach((entry) => {
 
-    //currentTimestamp
-    const currentTimestamp=uniqueTimestamps.shift();
+    if (!entry.title) {
+      console.log('No valid "title" array found in entry');
+      return;
+    }
 
-    //generateUUID
-    const generateUUID =await getuuid(currentTitle);
+    const titleObj = entry.title[0];
+    if (!titleObj && titleObj._) {
+      console.log("there is no title founded")
+      return;
+    }
 
-    //getting TimeZone
-    const utcTimestamp = await convertToUTC(currentTimestamp);
+    let locTitle = titleObj._;
+    const timestamp = entry.updated && entry.updated[0] ? entry.updated[0] : new Date().now().toISOString();
 
-    //getting lat and long
-    const { lat, long } = await lookupAddress(currentTitle);
+    locTitle = locTitle.replace("Active Fire Call at", " ").trim();
 
-    console.log( {
-    UUID: generateUUID, 
-    title: currentTitle, 
-    timestamps: utcTimestamp,
-    lat:lat,
-    long:long
-    });
-   }
-  }catch(err){
-  console.log(err)
+    //generating UUID
+    const alertId = getuuid(locTitle);
+
+    if (!seenTitles.has(locTitle)) {
+      seenTitles.add(locTitle);
+
+      uniqueAlertTitles.push({
+        id: alertId,
+        title: locTitle,
+        timestamp: timestamp
+      });
+    }
+
+  });
+  return uniqueAlertTitles;
+}
+
+async function processAlert(Alert) {
+  const alertId = Alert.id;
+  try {
+      //getting TimeZone
+      const utcTimestamp = convertToUTC(Alert.timestamp);
+
+      //getting lat and long
+      const { lat, long } = await lookupAddress(Alert.title);
+
+      //mapss
+      // const screenshot = await mapScreenShot(alertId, long, lat, 'NFFS');
+
+      return {
+        UUID: alertId,
+        lat: lat,
+        long: long,
+        timestamps: utcTimestamp,
+        title:Alert.title,
+        eventColor:'#8B0000',
+        eventType:'Fire Alert',
+      //mapScreenShot:screenshot,
+        confidenceRating:'High',
+        reportBy: "Niagara Falls Fire Department",
+      }
+  } catch (error) {
+    console.error('Error processing NiagaraFalls alert:', error);
+    await insertFailedAlertInRedis(alertId,Alert,"NIAGARAFALLS", error.message);
+    throw error;
   }
 }
 
@@ -102,12 +113,15 @@ function convertToUTC(timestamp) {
 //googlemapAPI
 const lookupAddress = async (roadNames) => {
   const cleanedStreet = cleanAddress(roadNames);
-  const searchQuery = `${cleanedStreet}, Niagara Falls, Ontario, Canada`;
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(searchQuery)}&key=${GOOGLE_API_KEY}&components=country:CA|administrative_area:ON|locality:Niagara%20Falls`;
+  const searchQuery = [`${cleanedStreet}, Niagara Falls, Ontario, Canada`];
+
+  for(const query of searchQuery){
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(query)}&key=${GOOGLE_API_KEY}&components=country:CA|administrative_area:ON|locality:Niagara%20Falls`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
+
     if (data.status === 'OK' && data.results.length > 0) {
       const { location } = data.results[0].geometry;
       const result = {
@@ -120,6 +134,7 @@ const lookupAddress = async (roadNames) => {
   } catch (error) {
     console.error(`Error looking up address for query "${roadNames}":`, error);
   }
+}
   throw new Error('Failed to find location after multiple attempts');
 };
 
@@ -148,15 +163,17 @@ const cleanAddress = (address) => {
 
 
 //main
-async function main(token ="",url="",) {
-  try{
-  const jsonData = await fetchXmlConvertJson();
-  const result =await processAlert(jsonData);
-  console.log(result)
-
-} catch (error) {
-  console.error("An error occurred:", error);
-}
+async function main(token = "") {
+  try {
+    const jsonData = await fetchDataFromFireAlertRSSFeed();
+    const unqiueAlerts = await getFireAlertUniqueLocationTitle(jsonData);
+    const formattedData = await Promise.all(unqiueAlerts.map( async (Alert) => {
+    return await processAlert(Alert)
+   }));
+    console.log("result",formattedData)
+  } catch (error) {
+    console.error("[NIAGARA_API] Error occurred :: ", error);
+  }
 }
 main();
 
